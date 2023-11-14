4a35dcaf3a480019bae36d2ef24d52e08bd0017d
stpafk <stephan.weigert@hotmail.com>
2023-11-14 14:42:08 -0300

    add mongodb

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+require("dotenv").config;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+/*
+CONNECT TO MONGODB
+*/
+
+const mongoDB = process.env.MONGODB
+
+mongoose.set("strictQuery", false);
+async function main() {
+  await mongoose.connect(mongoDB)
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
