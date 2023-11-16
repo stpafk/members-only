725d8aeda9d30a33bc8a9685422e3a26bfceaae9
stpafk <stephan.weigert@hotmail.com>
2023-11-16 10:22:54 -0300

    fix: users

--- models/users.js
+++ models/users.js
@@ -12,7 +12,7 @@ const UserSchema = new Schema({
 })
 
 UserSchema.virtual("full_name").get(function() {
-    return `${first_name} ${last_name}`;
+    return `${this.first_name} ${this.last_name}`;
 });
 
 module.exports = mongoose.model("User", UserSchema);
