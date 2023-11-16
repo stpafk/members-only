9dd316fead60c0cb82521089516ae3777ae0de3a
stpafk <stephan.weigert@hotmail.com>
2023-11-16 09:39:09 -0300

    add routes to index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const sessionController = require('../controllers/sessionController');
+const passport = require('passport');
+const authLogin = require('../config/passport')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', sessionController.get_index);
+
+router.get("/register", sessionController.get_register);
+router.post("/register", sessionController.post_register);
+
+router.get("/login", sessionController.get_login);
+router.post("/login", passport.authenticate("login"), function(req, res) {
+  res.redirect("/")
+})
 
 module.exports = router;
