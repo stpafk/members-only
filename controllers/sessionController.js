a8f951fc4439ea807f82517c53b4cd1eb4b1eec5
stpafk <stephan.weigert@hotmail.com>
2023-11-16 09:27:41 -0300

    feat: register POST

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const {body, validationResult} = require('express-validator');
-const User = require('../models/users')
+const bcrypt = require('bcryptjs');
+const User = require('../models/users');
 
 exports.get_index = asyncHandler(async(req, res, next) => {
     res.render("index", {
@@ -10,26 +11,27 @@ exports.get_index = asyncHandler(async(req, res, next) => {
 });
 
 exports.get_register = asyncHandler(async(req, res, next) => {
-    res.render("index", {
+    res.render("register", {
         title: "Register",
         req: req,
     })
 });
 
 exports.post_register = [
-    
+
     body("first_name").trim().isLength({min: 2, max: 50}).escape("Invalid first name."),
     body("last_name").trim().isLength({min: 2, max: 50}).escape("Invalid last name."),
     body("email").custom(async value => {
 
-        const user = await User.find({email: value});
+        const userEmail = await User.find({email: value});
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i;
 
-        if(user) {
+        if(userEmail.length > 0) {
+            console.log(userEmail)
             throw new Error("Email already registered.");
         };
-        
-        if(!emailRegex.test()) {
+
+        if(!emailRegex.test(value)) {
             throw new Error("Invalid email.");
         }
 
@@ -37,10 +39,31 @@ exports.post_register = [
     body("password").isLength({min: 6}).escape("Password length should be bigger than 6."),
     body("ps_confirm").custom((value, {req}) => {
         return value === req.body.password;
-    }),
+    }).escape("Passwords do not match"),
 
     asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
 
+        const password = await bcrypt.hash(req.body.password, 12);
+
+        const user = new User({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: password,
+        })
+
+        if(!errors.isEmpty()) {
+            res.render("register", {
+                title: "Register",
+                user: user,
+                errors: errors.array(),
+            });
+            return;
+        };
+
+        await user.save();
+        res.redirect("/")
     })
 ]
 
